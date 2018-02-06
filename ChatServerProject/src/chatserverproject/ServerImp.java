8ece875a80baab530097d48a59977f641f7f3972
hesham <mheshamg@gmail.com>
2018-02-06 09:08:49 -0800

    first commit

---
+++ ChatServerProject/src/chatserverproject/ServerImp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatserverproject;
+
+import chatprojectcommon.ServerInterface;
+
+/**
+ *
+ * @author mohamed hesham
+ */
+public class ServerImp implements ServerInterface{
+    
+}
