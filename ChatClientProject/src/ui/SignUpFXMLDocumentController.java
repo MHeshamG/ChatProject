92f9d42a716339652ec08b87ba0ed9b5d7382d64
hesham <mheshamg@gmail.com>
2018-02-06 08:47:26 -0800

    first commit

--- ChatClientProject/src/ui/SignUpFXMLDocumentController.java
+++ ChatClientProject/src/ui/SignUpFXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chatclientproject;
+package ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author mohamed hesham
  */
-public class FXMLDocumentController implements Initializable {
+public class SignUpFXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
