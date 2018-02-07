d71e30d7b250399815146f90676613737c27b82d
esraasalah <esraasalah937@gmail.com>
2018-02-07 09:09:25 +0200

    edit DatabaseHandler

--- ChatServerProject/src/database/DatabaseHandler.java
+++ ChatServerProject/src/database/DatabaseHandler.java
@@ -6,11 +6,20 @@
 package database;
 
 import java.sql.*; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class DatabaseHandler 
 {    
     
     private static DatabaseHandler databaseHandlerObj;
-    private DatabaseHandler(){};
+    private DatabaseHandler(){
+    
+       connect();
+    
+    };
+    
+      Connection con=null;
+      Statement stmt;
     
     public static DatabaseHandler  getInstance()
     {
@@ -21,32 +30,89 @@ public class DatabaseHandler
     }
     
     
+    public Connection connect()
+    {
+        
+       
+          
+        try{
+              Class.forName("com.mysql.jdbc.Driver");
+              con =DriverManager.getConnection(  
+                                                  "jdbc:mysql://localhost:3306/chatdatabase","root","root");
+              
+              stmt =con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        
+  
+        }
+    catch(SQLException e ){
+          
+        e.printStackTrace();
+    
+    }
+        
+    catch(Exception ex){
+          
+         ex.printStackTrace();
+    }  
+        
+    return con;
+    }
+    
+    
     public void insert(String query)
     {
     
-          
+        try {
+            stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     
     }
+    
+    
     
     public void update (String query)
     {
       
-           
+          try {
+            stmt.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }  
         
     }
+    
+    
+    
+    
     
       public void delete  (String query)
     {
     
-            
+        try {
+            stmt.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-         public  void  select(String query) 
+      
+      
+      
+      
+      
+         public  ResultSet  select(String query) 
     {
-    
+          ResultSet selectedData=null;
        
-        
-                
+          try {
+                 selectedData=stmt.executeQuery(query);
+          } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+          }
+          
+          return selectedData ;
                 
     }
     
